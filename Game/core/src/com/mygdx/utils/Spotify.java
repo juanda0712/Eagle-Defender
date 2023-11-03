35be82ae7bb28d0baca9079684e2fb1bdf7abef2
Juan Rodriguez <juanrm071299@gmail.com>
2023-11-02 20:40:52 -0600

    camera takepicture and save user with picture

--- Game/core/src/com/mygdx/utils/Spotify.java
+++ Game/core/src/com/mygdx/utils/Spotify.java
@@ -2,16 +2,20 @@ package com.mygdx.utils;
 
 import com.mygdx.models.SongInfo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 
 public class Spotify {
     public static void main(String[] args) {
-        SpotifyAuthenticator spotify = new SpotifyAuthenticator();
+        System.out.println(generateUniqueFilename("foto", "png"));
+    }
 
-        //obtine el analisis de la cancion
-        SongInfo info = spotify.getSongInfo("billi+jean");
-        System.out.println(info);
-
-        /*String playSong = auth.playSong("jazz");
-        System.out.println(playSong);*/
+    public static String generateUniqueFilename(String baseName, String extension) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMddHHmmss");
+        String timestamp = dateFormat.format(new Date());
+        String uniqueFilename = baseName + "_" + timestamp + "." + extension;
+        return uniqueFilename;
     }
 }
